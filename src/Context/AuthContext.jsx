027f155751b5ffa722a20216c44d32230edfc169
manonkeeman <manonkeeman@gmail.com>
2025-02-24 22:55:52 +0100

    Fix: AuthContext bestand toegevoegd en import gecorrigeerd

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,12 +1,12 @@
 import React, { createContext, useState, useContext } from "react";
 
-// Maak een context
+// Maak een context aan
 const AuthContext = createContext();
 
-// Custom hook voor gemakkelijke toegang tot AuthContext
+// Custom hook om AuthContext gemakkelijk te gebruiken
 export const useAuth = () => useContext(AuthContext);
 
-// AuthProvider component voor het beheren van inlogstatus
+// AuthProvider component om inlogstatus bij te houden
 export const AuthProvider = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
