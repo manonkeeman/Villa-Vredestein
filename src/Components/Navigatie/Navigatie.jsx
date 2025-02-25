1a3444c53ec787dfae7614edc24a6f84f9ee22a6
manonkeeman <manonkeeman@gmail.com>
2025-02-25 15:51:46 +0100

    Feature: Login link vervangen door user icoon en social media links bijgewerkt

--- src/Components/Navigatie/Navigatie.jsx
+++ src/Components/Navigatie/Navigatie.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { useAuth } from "../../Context/AuthContext";
+import { FaInstagram, FaWhatsapp, FaUser } from "react-icons/fa";
 import "./Navigatie.css"; // Zorg dat je CSS-bestand is gekoppeld
 
 export default function Navigatie() {
@@ -14,6 +15,7 @@ export default function Navigatie() {
   return (
       <nav className="navigatie-container">
         <h3 className="logo">Villa Vredestein</h3>
+
         {/* Hamburger-menu voor mobiel */}
         <div className={`hamburger ${menuOpen ? "open" : ""}`} onClick={toggleMenu}>
           <div className="bar"></div>
@@ -21,19 +23,33 @@ export default function Navigatie() {
           <div className="bar"></div>
         </div>
 
-        {/* Navigatielinks */}
-        <ul className={`nav-links ${menuOpen ? "show" : ""}`}>
-          <li><NavLink to="/" className="default-link">Home</NavLink></li>
-          <li><NavLink to="/about" className="default-link">About</NavLink></li>
-          <li><NavLink to="/contact" className="default-link">Contact</NavLink></li>
-          <li><NavLink to="/login" className="default-link">Login</NavLink></li>
-          {isLoggedIn && (
-              <>
-                <li><NavLink to="/register" className="default-link">Dashboard</NavLink></li>
-                <li><button onClick={logout} className="default-link">Logout</button></li>
-              </>
-          )}
-        </ul>
+          {/* Navigatielinks met iconen */}
+          <ul className={`nav-links ${menuOpen ? "show" : ""}`}>
+              <li><NavLink to="/" className="default-link">Home</NavLink></li>
+              <li><NavLink to="/about" className="default-link">About</NavLink></li>
+              <li><NavLink to="/contact" className="default-link">Contact</NavLink></li>
+              <li><NavLink to="/login" className="default-link">Login</NavLink></li>
+
+              {/* Social media icons */}
+              <li>
+                  <a href="https://www.instagram.com/villavredestein" target="_blank" rel="noopener noreferrer" className="icon-link">
+                      <FaInstagram />
+                  </a>
+              </li>
+              <li>
+                  <a href="https://wa.me/31625015299" target="_blank" rel="noopener noreferrer" className="icon-link">
+                      <FaWhatsapp />
+                  </a>
+              </li>
+              <li>
+
+                  {/* User icon that links to login */}
+                  <li>
+                      <NavLink to="/login" className="icon-link">
+                          <FaUser />
+                      </NavLink>
+                  </li>
+          </ul>
       </nav>
-  );
+);
 }
