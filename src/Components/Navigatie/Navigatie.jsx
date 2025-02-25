d9bbb1837b0e6ed3d7f4cdfd5097c0b0e9331224
manonkeeman <manonkeeman@gmail.com>
2025-02-25 16:22:25 +0100

    Fix: Navigatie en iconen goed gepositioneerd, hamburgermenu naar links verschoven

--- src/Components/Navigatie/Navigatie.jsx
+++ src/Components/Navigatie/Navigatie.jsx
@@ -1,55 +1,53 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { useAuth } from "../../Context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 import { FaInstagram, FaWhatsapp, FaUser } from "react-icons/fa";
-import "./Navigatie.css"; // Zorg dat je CSS-bestand is gekoppeld
+import "./Navigatie.css";
 
 export default function Navigatie() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const { isLoggedIn, logout } = useAuth();
+    const [menuOpen, setMenuOpen] = useState(false);
+    const { isLoggedIn, logout } = useAuth();
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
-  return (
-      <nav className="navigatie-container">
-        <h3 className="logo">Villa Vredestein</h3>
+    return (
+        <nav className="navigatie-container">
+            <h3 className="logo">Villa Vredestein</h3>
 
-        {/* Hamburger-menu voor mobiel */}
-        <div className={`hamburger ${menuOpen ? "open" : ""}`} onClick={toggleMenu}>
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
-        </div>
+            {/* Hamburger-menu icoon zichtbaar op mobiel */}
+            <div className={`hamburger ${menuOpen ? "open" : ""}`} onClick={toggleMenu}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </div>
 
-          {/* Navigatielinks met iconen */}
-          <ul className={`nav-links ${menuOpen ? "show" : ""}`}>
-              <li><NavLink to="/" className="default-link">Home</NavLink></li>
-              <li><NavLink to="/about" className="default-link">About</NavLink></li>
-              <li><NavLink to="/contact" className="default-link">Contact</NavLink></li>
-              <li><NavLink to="/login" className="default-link">Login</NavLink></li>
+            {/* Navigatielinks met iconen */}
+            <ul className={`nav-links ${menuOpen ? "show" : ""}`}>
+                <li><NavLink to="/" className="default-link">Home</NavLink></li>
+                <li><NavLink to="/about" className="default-link">About</NavLink></li>
+                <li><NavLink to="/contact" className="default-link">Contact</NavLink></li>
 
-              {/* Social media icons */}
-              <li>
-                  <a href="https://www.instagram.com/villavredestein" target="_blank" rel="noopener noreferrer" className="icon-link">
-                      <FaInstagram />
-                  </a>
-              </li>
-              <li>
-                  <a href="https://wa.me/31625015299" target="_blank" rel="noopener noreferrer" className="icon-link">
-                      <FaWhatsapp />
-                  </a>
-              </li>
-              <li>
+                {/* Social media icons */}
+                <li>
+                    <a href="https://www.instagram.com/villa.vredestein" target="_blank" rel="noopener noreferrer" className="icon-link">
+                        <FaInstagram />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://wa.me/31625015299" target="_blank" rel="noopener noreferrer" className="icon-link">
+                        <FaWhatsapp />
+                    </a>
+                </li>
 
-                  {/* User icon that links to login */}
-                  <li>
-                      <NavLink to="/login" className="icon-link">
-                          <FaUser />
-                      </NavLink>
-                  </li>
-          </ul>
-      </nav>
-);
+                {/* User icon linking to login */}
+                <li>
+                    <NavLink to="/login" className="icon-link">
+                        <FaUser />
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
 }
