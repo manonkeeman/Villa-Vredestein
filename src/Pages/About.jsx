185c3af8bf17d8e172302754b18635844ca5b985
manonkeeman <manonkeeman@gmail.com>
2025-03-05 16:27:25 +0100

    BentoBox on Home About Contact Login

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import BentoBox from "../Components/BentoBox/BentoBox.jsx";
+import "./About.css";
 
-function About() {
-  return <h1>De Vredesteiners</h1>;
-}
+const About = () => {
+  return (
+      <div>
+        <BentoBox/>
+      </div>
+  );
+};
+
 export default About;
