61c2f2e67b9342480d4deaec217fce937fa63bf0
manonkeeman <manonkeeman@gmail.com>
2025-03-05 16:16:41 +0100

    BentoBox component toegevoegd

---
+++ src/Components/BentoBox/BentoBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './BentoBox.css';
+
+const BentoBox = () => {
+    return (
+        <>
+            <div className="bento-grid">
+                <div className="box box1">Box 1</div>
+                <div className="box box2">Box 2</div>
+            </div>
+
+            {/* Onderste rij correct gescheiden in 3 kolommen */}
+            <div className="bento-grid-onderste">
+                <div className="box box3">Box 3</div>
+                <div className="box box4">Box 4</div>
+                <div className="box box5">Box 5</div>
+            </div>
+        </>
+    );
+};
+
+export default BentoBox;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,21 +1,12 @@
 import React from "react";
+import BentoBox from "../Components/BentoBox/BentoBox.jsx";
 import "./Home.css";
 
 const Home = () => {
     return (
-        <>
-            <div className="bento-grid">
-                <div className="box box1">Box 1</div>
-                <div className="box box2">Box 2</div>
-            </div>
-
-            {/* Onderste rij correct gescheiden in 3 kolommen */}
-            <div className="bento-grid-onderste">
-                <div className="box box3">Box 3</div>
-                <div className="box box4">Box 4</div>
-                <div className="box box5">Box 5</div>
-            </div>
-        </>
+        <div>
+            <BentoBox/>
+        </div>
     );
 };
 
