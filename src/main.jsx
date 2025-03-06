0a1f9fff2688135458729b4c3286a2a372ec0067
manonkeeman <manonkeeman@gmail.com>
2025-03-06 18:16:10 +0100

    clean up code

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider, useAuth } from "./context/AuthContext";
-import App from "./App";
 import FontLoader from "./Components/FontLoader/Fonts.jsx";
 import Navigatie from "./Components/Navigatie/Navigatie.jsx";
 import Home from "./Pages/Home.jsx";
 import About from "./Pages/About.jsx";
 import Contact from "./Pages/Contact.jsx";
 import Login from "./Pages/Login.jsx";
-import Register from "./Pages/Register.jsx";
+import Recipe  from "./Pages/Recipe.jsx";
 import NotFound from "./Pages/NotFound.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <AuthProvider>
             <BrowserRouter>
                 <FontLoader />
                 <Navigatie />
@@ -24,22 +20,9 @@ root.render(
                     <Route path="/about" element={<About />} />
                     <Route path="/contact" element={<Contact />} />
                     <Route path="/login" element={<Login />} />
-                    <Route
-                        path="/register"
-                        element={
-                            <PrivateRoute>
-                                <Register />
-                            </PrivateRoute>
-                        }
-                    />
+                    <Route path="/recipe" element={<Recipe />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </BrowserRouter>
-        </AuthProvider>
     </React.StrictMode>
 );
-
-function PrivateRoute({ children }) {
-    const { isLoggedIn } = useAuth();
-    return isLoggedIn ? children : <Login />;
-}
