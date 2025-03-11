f318efb534cc5d496bda8e65c28f980fe44c93eb
manonkeeman <manonkeeman@gmail.com>
2025-03-11 16:58:38 +0100

    verhuizing voor clean code en routing via main.jsx

--- src/Components/BentoBox/BentoBox.jsx
+++ src/Components/BentoBox/BentoBox.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './BentoBox.css';
+import VillaVredestein from "../../Assets/Images/VillaVredestein.jpeg";
 
 const BentoBox = () => {
     return (
@@ -7,9 +8,9 @@ const BentoBox = () => {
             <div className="bento-grid">
                 <div className="box box1">Box 1</div>
                 <div className="box box2">Box 2</div>
+                <img src={VillaVredestein} alt="Villa Vredestein" className="Villa Vredestein"/>
             </div>
 
-            {/* Onderste rij correct gescheiden in 3 kolommen */}
             <div className="bento-grid-onderste">
                 <div className="box box3">Box 3</div>
                 <div className="box box4">Box 4</div>
