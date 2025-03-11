68fb2016c5dcb87d58d46b6671a8b52e20400c91
manonkeeman <manonkeeman@gmail.com>
2025-03-11 16:58:38 +0100

    verhuizing voor clean code en routing via main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
 import FontLoader from "./Components/FontLoader/Fonts.jsx";
-import Navigatie from "./Components/Navigatie/Navigatie.jsx";
 import Home from "./Pages/Home.jsx";
 import About from "./Pages/About.jsx";
 import Contact from "./Pages/Contact.jsx";
@@ -14,14 +14,15 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
             <BrowserRouter>
                 <FontLoader />
-                <Navigatie />
                 <Routes>
+                    <Route path="/" element={<App />}>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/contact" element={<Contact />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/recipe" element={<Recipe />} />
                     <Route path="*" element={<NotFound />} />
+                    </Route>
                 </Routes>
             </BrowserRouter>
     </React.StrictMode>
