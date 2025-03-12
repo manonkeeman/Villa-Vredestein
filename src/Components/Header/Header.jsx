05413f7aa7b0f4aab440d09bb651ea835aedc485
manonkeeman <manonkeeman@gmail.com>
2025-03-12 11:24:40 +0100

    header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Navigatie from "./Navigatie/Navigatie.jsx";
-import "./Navigatie/Navigatie.css";
+import Navigatie from "../Navigatie/Navigatie.jsx";
+import "../Navigatie/Navigatie.css";
 import "./Header.css";
 
 const Header = () => {
