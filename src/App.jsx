ec99f994796e360e23d07c6dd15a2518a4bcc2f9
manonkeeman <manonkeeman@gmail.com>
2025-03-16 22:09:37 +0100

    page structuur en design

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Components/Header/Header.jsx";
-import BentoBox from "./Components/BentoBox/BentoBox";
 import "./App.css";
 
 function App() {
     return (
-       <>
+        <>
             <Header />
             <Outlet />
-      </>
+        </>
     );
 }
+
 export default App;
--- src/Components/BentoBox/BentoBox.jsx
+++ src/Components/BentoBox/BentoBox.jsx
@@ -1,42 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './BentoBox.css';
-import Button from "../Buttons/Button";
-import VillaVredestein from "../../Assets/Images/VillaVredestein.jpeg";
-import InkijkjeinVredestein from "../../Assets/Images/InkijkjeinVredestein.jpeg";
 
-const BentoBox = () => {
+const BentoBox = ({ content }) => {
     return (
         <div className="wrapper">
             <div className="bento-grid">
-
-                <div className="box box1">
-                    <h2>Welkom bij Villa Vredestein</h2>
-                    <Button text="Lees meer"/>
-                </div>
-
-                <div className="box box2">
-                    <img src={VillaVredestein} alt="Villa Vredestein"/>
-                </div>
-
-                <div className="box box3">
-                    <img src={InkijkjeinVredestein} alt="Inkijkje in Villa Vredestein"/>
-                </div>
-
-                <div className="box box4">
-                    <p>Hier komt tekst</p>
-                    <Button text="Meer info"/>
-                </div>
-
-                <div className="box box5">
-                    <video autoPlay loop muted playsInline width="100%">
-                        <source src="/Videos/MaximEnManonInSuburban.mp4" type="video/mp4"/>
-                        Your browser does not support the video tag.
-                    </video>
-                </div>
-
+                {content}
             </div>
         </div>
     );
+};
+
+BentoBox.propTypes = {
+    content: PropTypes.node.isRequired,
 };
 
 export default BentoBox;
