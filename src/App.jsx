5a1e8e381301d7375ce474c74de8405afd1f63b3
manonkeeman <manonkeeman@gmail.com>
2025-03-25 17:49:30 +0100

    responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
 import Header from "./Components/Header/Header.jsx";
 import "./App.css";
+import {Route, Routes} from "react-router-dom";
+import Home from "./Pages/Home.jsx";
+import About from "./Pages/About.jsx";
+import Contact from "./Pages/Contact.jsx";
+import Login from "./Pages/Login.jsx";
+import Recipe from "./Pages/Recipe.jsx";
+import NotFound from "./Pages/NotFound.jsx";
 
 function App() {
     return (
         <>
-            <Header />
-            <Outlet />
+            <Header/>
+            <Routes>
+                <Route index element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/recipe" element={<Recipe/>}/>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
         </>
     );
 }
