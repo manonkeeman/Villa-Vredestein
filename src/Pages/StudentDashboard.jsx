2a934c8baf90ac91038d33e0f310f156771ef318
manonkeeman <manonkeeman@gmail.com>
2025-04-08 11:50:36 +0200

    voeg auth/loginpage toe

---
+++ src/Pages/StudentDashboard.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useAuth } from "../Components/Auth/AuthContext";
+import { Navigate } from "react-router-dom";
+import "../Styles/BentoGrid.css";
+import "./StudentDashboard.css";
+import { Link } from "react-router-dom"
+
+const StudentDashboard = () => {
+    const { isLoggedIn } = useAuth();
+
+    if (!isLoggedIn) {
+        return <Navigate to="/login" replace />;
+    }
+
+    return (
+        <div className="StudentDashboard">
+            <div className="bento-grid">
+                <div className="box box1">
+                    <h1>Welkom Vredesteiner</h1>
+                </div>
+                <div className="box box2">
+                    <h1>Wat eten we vandaag</h1>
+                    <Link to="/dashboard/receptenzoeker">→ Naar receptenzoeker</Link>
+                </div>
+                <div className="box box3">
+                    <h1>Informatie gegevens</h1>
+                </div>
+                <div className="box box4">
+                    <h1>Schoonmaaktaken</h1>
+                </div>
+                <div className="box box5">
+                    <h1>Heb ik mijn huur betaald?</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default StudentDashboard;
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext.jsx";
-import FontLoader from "./Components/FontLoader/Fonts.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./Components/Auth/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <AuthProvider>
-            <BrowserRouter>
-                <FontLoader />
-                <App/>
-            </BrowserRouter>
-        </AuthProvider>
+        <BrowserRouter>
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </BrowserRouter>
     </React.StrictMode>
 );
