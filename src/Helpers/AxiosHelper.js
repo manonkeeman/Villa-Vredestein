e9d28959c751f6cbf662ce92511db2a154d84f84
manonkeeman <manonkeeman@gmail.com>
2025-06-24 11:58:12 +0200

    Netlify contactformulier + JWT fixes in Dashboard en Auth

--- src/Helpers/AxiosHelper.js
+++ src/Helpers/AxiosHelper.js
@@ -9,5 +9,19 @@ const axiosInstance = axios.create({
     withCredentials: false,
 });
 
-export default axiosInstance;
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("accessToken");
 
+        if (token) {
+            config.headers["Authorization"] = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
