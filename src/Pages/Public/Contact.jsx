7f9fddffb4910d251c0f495d021cccbdb3addef9
manonkeeman <manonkeeman@gmail.com>
2025-06-25 19:39:59 +0200

    Initial commit naar GitHub

--- src/Pages/Public/Contact.jsx
+++ src/Pages/Public/Contact.jsx
@@ -22,20 +22,18 @@ const Contact = () => {
             <section className="map-box">
                 <iframe
                     title="Locatie Villa Vredestein"
-                    src="https://www.google.com/maps/embed?pb=!1m18!..."
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2460.1903295479915!2d5.287013376747164!3d52.04312207197006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c66d27b1c41c65%3A0x60714aaabf21804f!2sHoofdstraat%20147%2C%203975%20ED%20Driebergen-Rijsenburg!5e0!3m2!1snl!2snl!4v1719324930896!5m2!1snl!2snl"
                     loading="lazy"
                     allowFullScreen
                     referrerPolicy="no-referrer-when-downgrade"
-                />
+                ></iframe>
             </section>
 
-            <noscript>
                 <form name="contact" data-netlify="true" hidden>
                     <input type="text" name="naam" />
                     <input type="email" name="email" />
                     <textarea name="bericht" />
                 </form>
-            </noscript>
         </main>
     );
 };
