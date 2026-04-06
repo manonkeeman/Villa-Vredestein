036e97fc39ba7bbb84191cc0e5a9ba7fe55f75fa
manonkeeman <manonkeeman@gmail.com>
2026-04-06 13:32:20 +0200

    feat: add Carpe Diem Design blog card and article (7 languages)
    - New blog slug: carpe-diem-design in all 6 locales (nl/en/fr/de/es/it)
    - Added to imageMap in About.jsx and Blog.jsx (PannenkoekenAvond as placeholder)
    - Added to sitemap.xml with hreflang alternates (priority 0.8 / monthly)

--- src/Pages/Public/About.jsx
+++ src/Pages/Public/About.jsx
@@ -8,6 +8,7 @@ import Villa1 from "../../Assets/Images/VillaVredestein2024.jpg";
 import Villa2 from "../../Assets/Images/VillaVredestein1910.jpg";
 import Villa3 from "../../Assets/Images/VillaVredesteinRestauratie.jpg";
 import Villa6 from "../../Assets/Images/DeOmgevingVillaVredestein.jpg";
+import Villa7 from "../../Assets/Images/PannenkoekenAvondVillaVredestein.jpg";
 import Villa8 from "../../Assets/Images/Maxim_Manon_ChevroletSuburban.jpg";
 import Villa9 from "../../Assets/Images/BezoekOnsVillaVredestein.jpg";
 
@@ -16,6 +17,7 @@ const imageMap = {
     "geschiedenis": Villa2,
     "restauratie": Villa3,
     "omgeving": Villa6,
+    "carpe-diem-design": Villa7,
     "over-ons": Villa8,
     "bezoek-inspiratie": Villa9,
 };
